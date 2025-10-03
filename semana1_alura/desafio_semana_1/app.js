5c80df70da389c7be2b7cb232c39a384c84bc15a
Jairo Oliveira Santos <jairobr1986@gmail.com>
2025-10-03 08:01:10 -0300

    Semana 1
    Ainda na semana 1 Alura e Santander

--- semana1_alura/desafio_semana_1/app.js
+++ semana1_alura/desafio_semana_1/app.js
@@ -1,6 +1,1 @@
-alert("Boas vindas ao nosso site!");
-let nome = "Lua";
-let idade = 25;
-let numeroDeVendas = 50;
-let saldoDisponivel = 1000;
-let mensagemDeErro = "Erro! Preencha todos os campos"
+
--- semana1_alura/logica-js-projeto_inicial/app.js
+++ semana1_alura/logica-js-projeto_inicial/app.js
@@ -1,7 +1,8 @@
 alert("Game Number Secret!!!");
 let numeroSecreto = 7;
+console.log(numeroSecreto)
 let chute = prompt("Digite um número, entre 1 e 7: ");
 
 if (numeroSecreto == chute) {
-console.log()
-}
+    alert("Isso aí! Você acertou o número secreto (7)")
+} else { alert("Você errou!!! :(") }
