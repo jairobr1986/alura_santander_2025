f4a6a2d01d9bea34a02aaf9a9fc6232638d83bd9
Jairo Oliveira Santos <jairobr1986@gmail.com>
2025-10-03 07:25:53 -0300

    Santander e Alura Semana 1

---
+++ semana1_alura/desafio_semana_1_part2/app.js
@@ -0,0 +1,6 @@
+nome = prompt("Qual o seu nome?");
+idade = prompt("Qual a sua idade?");
+
+if (idade >= 18) {
+    alert(`Olá ${nome}, você tem ${idade} anos e Pode tirar a habilitação! `)
+} else {`Olá ${nome}, você deve esperar até fazer 18 anos ou mais.`}
---
+++ semana1_alura/logica-js-projeto_inicial/app.js
@@ -0,0 +1,7 @@
+alert("Game Number Secret!!!");
+let numeroSecreto = 7;
+let chute = prompt("Digite um número, entre 1 e 7: ");
+
+if (numeroSecreto == chute) {
+console.log()
+}
