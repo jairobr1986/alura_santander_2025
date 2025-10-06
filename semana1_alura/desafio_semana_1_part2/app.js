1d97b43a619ee72981ff21312492d31294ac83e5
Jairo Oliveira Santos <jairobr1986@gmail.com>
2025-10-05 22:26:24 -0300

    05.10.2025 semana1

--- semana1_alura/desafio_semana_1_part2/app.js
+++ semana1_alura/desafio_semana_1_part2/app.js
@@ -1,6 +1,5 @@
 let nome = prompt("Qual o seu nome?");
 let idade = prompt("Qual a sua idade?");
-
 if (idade >= 18) {
     alert(`Olá ${nome}, você tem ${idade} anos e Pode tirar a habilitação! `)
 } else {`Olá ${nome}, você deve esperar até fazer 18 anos ou mais.`}
