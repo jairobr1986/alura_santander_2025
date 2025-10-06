1e7a4ad69feacf26ad732ede97e9849bb141a3be
Jairo Oliveira Santos <jairobr1986@gmail.com>
2025-10-06 07:58:25 -0300

    ; faltou ponto e virgulas

--- semana1_alura/logica-js-aula_1_jairo_numeroSecreto/app.js
+++ semana1_alura/logica-js-aula_1_jairo_numeroSecreto/app.js
@@ -1,7 +1,7 @@
 alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 5;
 console.log(numeroSecreto);
-let chute 
+let chute ;
 
 while (chute != numeroSecreto) {
     chute = Number(prompt('Escolha um número entre 1 e 10'));
@@ -9,9 +9,9 @@ while (chute != numeroSecreto) {
         alert('Isso ai! Você descobriu o número secreto (5)');
     } else {
         if (chute > numeroSecreto) {
-            alert(` O seu chute ${chute} é MAIOR que o número secreto, TENTE NOVAMENTE!`)
+            alert(` O seu chute ${chute} é MAIOR que o número secreto, TENTE NOVAMENTE!`);
         } else {
-            alert(`O seu chute foi ${chute} ele é MENOR que o número secreto, TENTE NOVAMENTE!`)
+            alert(`O seu chute foi ${chute} ele é MENOR que o número secreto, TENTE NOVAMENTE!`);
         }
     }
 }
